cd713aeba6998999a082c313fbcf1502f1bcc9e4
Jen Nguyen <jknguyen@mit.edu>
2018-06-12 15:12:04 +0200

    monod and jensens expectations using growth rate in length

--- figure36.m
+++ figure36.m
@@ -32,7 +32,7 @@
 
 %  Last edit: Jen Nguyen, 2018 Jun 12
 
-%  Commit: monod and jensens expectations using growth rate in width
+%  Commit: monod and jensens expectations using growth rate in length
 
 
 
@@ -43,7 +43,7 @@
 
 % Strategy:
 
-%       0. initialize meta data and dV/dt stats
+%       0. initialize meta data and dL/dt stats
 %       0. initialize summary vectors for data
 %       1. for each experiment, collect values of G_low and G_high
 %       2. plot G_data 
@@ -55,7 +55,7 @@ clc
 
 cd('/Users/jen/Documents/StockerLab/Data_analysis/')
 load('storedMetaData.mat')
-load('dWdtData_fullOnly.mat') % this data uses instantaneous points present after 3hrs, from full curves only
+load('dLdtData_fullOnly.mat') % this data uses instantaneous points present after 3hrs, from full curves only
 
 
 % 0. initialize summary vectors for data
@@ -84,8 +84,8 @@ for e = 1:experimentCount
     low = 2;
     high = 4;
     
-    G_low(counter) = dWdtData_fullOnly{index}{1,low}.mean;
-    G_high(counter) = dWdtData_fullOnly{index}{1,high}.mean;
+    G_low(counter) = dLdtData_fullOnly{index}{1,low}.mean;
+    G_high(counter) = dLdtData_fullOnly{index}{1,high}.mean;
     
     timescales_perG(counter) = timescale;
     dates_perG{counter} = date;
@@ -95,11 +95,11 @@ end
 
 % 2. plot G_data
 figure(1)
-plot(G_low*3600,G_high*3600,'o','MarkerSize',10,'LineWidth',2);
+plot(G_low,G_high,'o','MarkerSize',10,'LineWidth',2);
 %axis([0 5 0 12])
-title('test for strong day-to-day correlations in dW/dt')
-xlabel('mean dW/dt in low')
-ylabel('mean dW/dt in high')
+title('test for strong day-to-day correlations in dL/dt')
+xlabel('mean dL/dt in low')
+ylabel('mean dL/dt in high')
 
 
 %% TWO. growth expectations: G_fluc vs timescale
@@ -108,12 +108,12 @@ ylabel('mean dW/dt in high')
 %       including lines for G_monod, G_jensen's, G_high and G_low
 %
 %       no correlation seen in part one, thus each fluctuating data point
-%       will be normalized to average dW/dt from entire stable data set
+%       will be normalized to average dL/dt from entire stable data set
 
 
 % Strategy: 
 %
-%       0. initialize meta data and dW/dt stats
+%       0. initialize meta data and dL/dt stats
 %       0. initialize summary vectors for data
 %       1. for each experiment, collect values of G_fluc, G_low, G_ave, G_high
 %       2. calculate mean G_low, mean G_ave, mean G_high and G_jensens
@@ -123,15 +123,15 @@ ylabel('mean dW/dt in high')
 %       4. plot G_data by timescale
 
 
-% 0. initialize meta data and dW/dt stats
+% 0. initialize meta data and dL/dt stats
 clear
 clc
 
 cd('/Users/jen/Documents/StockerLab/Data_analysis/')
 load('storedMetaData.mat')
-load('dWdtData_fullOnly.mat') % this data uses instantaneous points present after 3hrs, from full curves only
+load('dLdtData_fullOnly.mat') % this data uses instantaneous points present after 3hrs, from full curves only
                                          % units are in um per sec
-load('dWdtData_fullOnly_normalized.mat') % both datasets are generated and saved in figure34.m script
+load('dLdtData_fullOnly_normalized.mat') % both datasets are generated and saved in figure34.m script
 
 
 % 0. initialize summary vectors for data
@@ -174,16 +174,16 @@ for e = 1:experimentCount
     high = 4;
     
     % raw data
-    flucRates_raw(counter,fluc) = dWdtData_fullOnly{index}{1,fluc}.mean*3600;
-    stableRates_raw(counter,low) = dWdtData_fullOnly{index}{1,low}.mean*3600;
-    stableRates_raw(counter,ave) = dWdtData_fullOnly{index}{1,ave}.mean*3600;
-    stableRates_raw(counter,high) = dWdtData_fullOnly{index}{1,high}.mean*3600;
+    flucRates_raw(counter,fluc) = dLdtData_fullOnly{index}{1,fluc}.mean;%*3600;
+    stableRates_raw(counter,low) = dLdtData_fullOnly{index}{1,low}.mean;%*3600;
+    stableRates_raw(counter,ave) = dLdtData_fullOnly{index}{1,ave}.mean;%*3600;
+    stableRates_raw(counter,high) = dLdtData_fullOnly{index}{1,high}.mean;%*3600;
     
     % normalized data
-    flucRates_norm(counter,fluc) = dWdtData_fullOnly_normalized{index}{1,fluc}.mean*3600;
-    stableRates_norm(counter,low) = dWdtData_fullOnly_normalized{index}{1,low}.mean*3600;
-    stableRates_norm(counter,ave) = dWdtData_fullOnly_normalized{index}{1,ave}.mean*3600;
-    stableRates_norm(counter,high) = dWdtData_fullOnly_normalized{index}{1,high}.mean*3600;
+    flucRates_norm(counter,fluc) = dLdtData_fullOnly_normalized{index}{1,fluc}.mean;%*3600;
+    stableRates_norm(counter,low) = dLdtData_fullOnly_normalized{index}{1,low}.mean;%*3600;
+    stableRates_norm(counter,ave) = dLdtData_fullOnly_normalized{index}{1,ave}.mean;%*3600;
+    stableRates_norm(counter,high) = dLdtData_fullOnly_normalized{index}{1,high}.mean;%*3600;
     
     timescales_perG(counter) = timescale;
     dates_perG{counter} = date;
@@ -246,7 +246,7 @@ hold on
 %axis([-1 6 0 12])
 title('growth expectations')
 xlabel('fluctuating timescale')
-ylabel('mean dW/dt (um/hr)')
+ylabel('mean dL/dt (um/hr)')
 
 
 % raw, normalized by G_jensens
@@ -264,7 +264,7 @@ plot(6, G_jensens_raw/G_jensens_raw,'o','Color',rgb('SlateGray'),'MarkerSize',10
 %axis([-1 6 0 1.2])
 title('growth, relative to Jensens expectations')
 xlabel('fluctuating timescale')
-ylabel('mean dW/dt, normalized to G_jensens')
+ylabel('mean dL/dt, normalized to G_jensens')
 
 
 % dVdt normalized by initial vol
@@ -282,7 +282,7 @@ hold on
 %axis([-1 6 0 2])
 title('growth expectations')
 xlabel('fluctuating timescale')
-ylabel('mean dW/dt / W (1/hr)')
+ylabel('mean dL/dt / L (1/hr)')
 
 
 % dVdt normalized by initial vol, normalized by G_jensens
@@ -300,5 +300,5 @@ plot(6, G_jensens_norm/G_jensens_norm,'o','Color',rgb('SlateGray'),'MarkerSize',
 %axis([-1 6 0 1.6])
 title('growth, relative to Jensens expectations')
 xlabel('fluctuating timescale')
-ylabel('mean dW/dt / W, normalized to G_jensens')
+ylabel('mean dL/dt / L, normalized to G_jensens')
 
