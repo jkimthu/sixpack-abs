3fa30dd5cfe9023a69dfce387786a561b06d18da
Jen Nguyen <jknguyen@mit.edu>
2018-06-13 15:56:23 +0200

    revisited while working on Figure33. edited method of IDing unique cell cycles

--- figure1_2.m
+++ figure1_2.m
@@ -4,8 +4,9 @@
 %  Goal: determine whether growth in low, ave, high and fluc environments
 %        are characterizable as adder, sizer, or timer
 
-%        plot (1) population-averaged division size vs birth size
-%        plot (2) population-averaged inter-division time vs birth time
+%        plot (1) population-averaged division volume vs birth volume
+%        plot (2) population-averaged interdivision time vs birth volume
+
 
 
 %  Strategy: 
@@ -18,8 +19,10 @@
 
 
 
-%  Last edit: jen, 2018 May 2
-%  Commit: add expected line of V_div = 2 * V_birth on figure 1
+%  Last edit: jen, 2018 June 13
+
+%  Commit: revisited while working on Figure33. edited method of IDing
+%  unique cell cycles.
 
 
 %  OK let's go!
@@ -101,69 +104,76 @@ for e = 1:experimentCount
         % 9. isolate volume and interdiv time
         interdivTime = conditionData_trim(:,8)/60;     % col 8  = curve duration (sec converted to min)
         volume = conditionData_trim(:,12);             % col 12  = volume (Va)
+        curveFinder = conditionData_trim(:,6);    % col 6 = curveFinder, ID of full cell cycles
         
         
-        % 11. identify unique interdivision times (unique cell cycles)
-        unique_interdivs = unique(interdivTime);
+        % 10. identify unique unique cell cycles
+        curveIDs = unique(curveFinder);
         
         
-        % 12. for each unique cell cycle, calculate C+D period (min)
-        V_division = nan(length(unique_interdivs),1);
-        V_birth = nan(length(unique_interdivs),1);
+        % 11. for each unique, store volume at birth and divsion
+        V_division = nan(length(curveIDs),1);
+        V_birth = nan(length(curveIDs),1);
         
         for cc = 1:length(unique_interdivs)
             
-            currentVolumes = volume(interdivTime == unique_interdivs(cc));
+            currentVolumes = volume(curveFinder == curveIDs(cc));
             V_division(cc,1) = currentVolumes(end);
             V_birth(cc,1) = currentVolumes(1);
             
         end
-    
-    color = rgb(palette(condition));
-    
-    if condition == 1 && timescale == 300
-        xmark = shapes{2};
-    elseif condition == 1 && timescale == 900
-        xmark = shapes{3};
-    elseif condition == 1 && timescale == 3600
-        xmark = shapes{4};
-    else
-        xmark = shapes{1};
+        
+        
+        % 11. initialize plotting parameters
+        color = rgb(palette(condition));
+        
+        if condition == 1 && timescale == 300
+            xmark = shapes{2};
+        elseif condition == 1 && timescale == 900
+            xmark = shapes{3};
+        elseif condition == 1 && timescale == 3600
+            xmark = shapes{4};
+        else
+            xmark = shapes{1};
+        end
+        
+        
+        % 12. calculate mean and standard deviation of birth and division volume
+        V_birth_means = mean(V_birth);
+        V_div_means = mean(V_division);
+        interdiv_means = mean(unique_interdivs);
+        
+        V_birth_stds = std(V_birth);
+        V_div_stds = std(V_division);
+        interdiv_stds = std(unique_interdivs);
+        
+        
+        % 13. plot!
+        % (i) division size vs. birth size
+        figure(1)
+        errorbar(V_birth_means,V_div_means,V_div_stds,'Color',color)
+        hold on
+        plot(V_birth_means,V_div_means,'Marker',xmark,'Color',color)
+        hold on
+        xlabel('birth size (cubic um)')
+        ylabel('division size (cubic um)')
+        title('population averages from all experiments')
+        axis([1 7 0 16])
+        
+        
+        % (ii) inter-division time vs. birth size
+        %     figure(2)
+        %     errorbar(V_birth_means,interdiv_means,interdiv_stds,'Color',color)
+        %     hold on
+        %     plot(V_birth_means,interdiv_means,'Marker',xmark,'Color',color)
+        %     hold on
+        %     xlabel('birth size (cubic um)')
+        %     ylabel('inter-division time (min)')
+        %     title('population averages from all experiments')
+        %     axis([0 10 0 100])
+        %
+        
     end
-    
-    V_birth_means = mean(V_birth);
-    V_div_means = mean(V_division);
-    interdiv_means = mean(unique_interdivs);
-    
-    V_birth_stds = std(V_birth);
-    V_div_stds = std(V_division);
-    interdiv_stds = std(unique_interdivs);
-    
-    % (i) division size vs. birth size
-    figure(1)
-    errorbar(V_birth_means,V_div_means,V_div_stds,'Color',color)
-    hold on
-    plot(V_birth_means,V_div_means,'Marker',xmark,'Color',color)
-    hold on
-    xlabel('birth size (cubic um)')
-    ylabel('division size (cubic um)')
-    title('population averages from all experiments')
-    axis([1 7 0 16])
-
-
-    % (ii) inter-division time vs. birth size
-%     figure(2)
-%     errorbar(V_birth_means,interdiv_means,interdiv_stds,'Color',color)
-%     hold on
-%     plot(V_birth_means,interdiv_means,'Marker',xmark,'Color',color)
-%     hold on
-%     xlabel('birth size (cubic um)')
-%     ylabel('inter-division time (min)')
-%     title('population averages from all experiments')
-%     axis([0 10 0 100])
-%     
-   
-    end  
     
     
 end
