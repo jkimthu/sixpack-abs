c089729c56919bd40bf68cc7f229212c2f193777
Jen Nguyen <jknguyen@mit.edu>
2018-08-26 17:46:03 +0200

    edit for interchangeable growth rate metrics, plot log

--- figure22.m
+++ figure22.m
@@ -4,19 +4,12 @@
 %       fluctuating timescale. plot values for Monod and Jensen's
 %       expectations, as well.
 %
-%       six output plots:
+%       output plots:
 %
 %           1. G_high vs G_low across experiments (part one)
+%           2. expectations, plotting growth rate normalized to G_monod
+%           3. expectations, plotting dV/dt values normalized to G_jensens
 
-%           2. expectations, plotting raw dV/dt values
-%           3. expectations, plotting raw dV/dt values normalized to G_jensens
-
-%           4. expectations, plotting dV/dt values normalized by initial volume
-%           5. expectations, plotting dV/dt values normalized by initial
-%              volume and G_jensens
-
-%           6. expectations, plotting dV/dt values normalized by initial 
-%              volume and G_monod
 
 
 
@@ -28,14 +21,14 @@
 %       1. plot G_high vs G_low: if correlated, then suggestive of important day-to-day variability
 %                                if not correlated, average of all experiments is fine
 %
-%       2. plot G_fluc (normalized as decided from step 1) vs timescale,
+%       2. plot G_fluc vs timescale,
 %          including G_monod and G_jensen's for comparison to expectable
 %          values
 
 
 
-%  Last edit: Jen Nguyen, 2018 Jul 7
-%  Commit: add plot normalizing growth rate (1/hr) by G_monod for poster
+%  Last edit: Jen Nguyen, 2018 Aug 26
+%  Commit: edit for interchangeable growth rate metrics 
 
 
 
@@ -116,7 +109,7 @@ ylabel('mean dV/dt in high')
 
 % Strategy: 
 %
-%       0. initialize meta data and dV/dt stats
+%       0. initialize meta data and growth rate stats
 %       0. initialize summary vectors for data
 %       1. for each experiment, collect values of G_fluc, G_low, G_ave, G_high
 %       2. calculate mean G_low, mean G_ave, mean G_high and G_jensens
@@ -132,19 +125,20 @@ clc
 
 cd('/Users/jen/Documents/StockerLab/Data_analysis/')
 load('storedMetaData.mat')
-load('dVdtData_fullOnly_newdVdt.mat') % this data uses instantaneous points present after 3hrs, from full curves only
-                                      % units are in cubic um per sec
-load('dVdtData_fullOnly_normalized_newdVdt.mat') % both datasets are generated and saved in figure21.m script
+load('growthRateData_fullOnly_log_wholeInteger.mat') % this data uses instantaneous points present after 3hrs, from full curves only
+                                                     % only whole integer hours are used to calculate stats
+                                                     % units = 1/hr, not yet divided by ln(2)
+                                                     % datasets are generated and saved in figure21.m script
 
 
 % 0. initialize summary vectors for data
 dataIndex = find(~cellfun(@isempty,storedMetaData));
-experimentCount = length(dataIndex);
+numExperiments = 16;
 
 
 % 1. for each experiment, collect values of G_low and G_high
 counter = 0;
-for e = 1:experimentCount
+for e = 1:numExperiments
     
     % identify experiment by date
     index = dataIndex(e);
@@ -157,16 +151,6 @@ for e = 1:experimentCount
         continue
     end
     
-%     if strcmp(date, '2018-01-12') == 1
-%         disp(strcat(date,': excluded from analysis'))
-%         continue
-%     end
-%     
-%     if strcmp(date, '2017-11-13') == 1
-%         disp(strcat(date,': excluded from analysis'))
-%         continue
-%     end
-
 
     disp(strcat(date, ': collecting data!'))
     counter = counter + 1;
@@ -177,17 +161,11 @@ for e = 1:experimentCount
     ave = 3;
     high = 4;
     
-    % raw data
-    flucRates_raw(counter,fluc) = dVdtData_fullOnly_newdVdt{index}{1,fluc}.mean*3600;
-    stableRates_raw(counter,low) = dVdtData_fullOnly_newdVdt{index}{1,low}.mean*3600;
-    stableRates_raw(counter,ave) = dVdtData_fullOnly_newdVdt{index}{1,ave}.mean*3600;
-    stableRates_raw(counter,high) = dVdtData_fullOnly_newdVdt{index}{1,high}.mean*3600;
-    
-    % normalized data
-    flucRates_norm(counter,fluc) = dVdtData_fullOnly_normalized_newdVdt{index}{1,fluc}.mean*3600;
-    stableRates_norm(counter,low) = dVdtData_fullOnly_normalized_newdVdt{index}{1,low}.mean*3600;
-    stableRates_norm(counter,ave) = dVdtData_fullOnly_normalized_newdVdt{index}{1,ave}.mean*3600;
-    stableRates_norm(counter,high) = dVdtData_fullOnly_normalized_newdVdt{index}{1,high}.mean*3600;
+    % growth rate data
+    flucRates(counter,fluc) = growthRateData_fullOnly{index}{1,fluc}.mean/log(2);
+    stableRates(counter,low) = growthRateData_fullOnly{index}{1,low}.mean/log(2);
+    stableRates(counter,ave) = growthRateData_fullOnly{index}{1,ave}.mean/log(2);
+    stableRates(counter,high) = growthRateData_fullOnly{index}{1,high}.mean/log(2);
     
     timescales_perG(counter) = timescale;
     dates_perG{counter} = date;
@@ -195,16 +173,13 @@ for e = 1:experimentCount
 end
 
 
-% 2. calculate mean G_low, mean G_ave, mean G_high and G_jensens
-stableRates_raw_mean = nanmean(stableRates_raw);
-stableRates_raw_std = nanstd(stableRates_raw);
-G_jensens_raw = (stableRates_raw_mean(high) + stableRates_raw_mean(low))/2;
-G_monod_raw = stableRates_raw_mean(ave);
 
-stableRates_norm_mean = nanmean(stableRates_norm);
-stableRates_norm_std = nanstd(stableRates_norm);
-G_jensens_norm = (stableRates_norm_mean(high) + stableRates_norm_mean(low))/2;
-G_monod_norm = stableRates_norm_mean(ave);
+% 2. calculate mean G_low, mean G_ave, mean G_high and G_jensens
+stableRates_mean = nanmean(stableRates);
+stableRates_std = nanstd(stableRates);
+G_jensens = (stableRates_mean(high) + stableRates_mean(low))/2;
+G_monod = stableRates_mean(ave);
+
 
 
 % 3. calculate mean dV/dt for each fluctuating timescale
@@ -213,118 +188,75 @@ t300 = 2;
 t900 = 3;
 t3600 = 4;
 
-Gfluc_raw_means(t30) = mean(flucRates_raw(timescales_perG==30));
-Gfluc_raw_means(t300) = mean(flucRates_raw(timescales_perG==300));
-Gfluc_raw_means(t900) = mean(flucRates_raw(timescales_perG==900));
-Gfluc_raw_means(t3600) = mean(flucRates_raw(timescales_perG==3600));
+Gfluc_means(t30) = mean(flucRates(timescales_perG==30));
+Gfluc_means(t300) = mean(flucRates(timescales_perG==300));
+Gfluc_means(t900) = mean(flucRates(timescales_perG==900));
+Gfluc_means(t3600) = mean(flucRates(timescales_perG==3600));
 
-Gfluc_raw_std(t30) = std(flucRates_raw(timescales_perG==30));
-Gfluc_raw_std(t300) = std(flucRates_raw(timescales_perG==300));
-Gfluc_raw_std(t900) = std(flucRates_raw(timescales_perG==900));
-Gfluc_raw_std(t3600) = std(flucRates_raw(timescales_perG==3600));
+Gfluc_std(t30) = std(flucRates(timescales_perG==30));
+Gfluc_std(t300) = std(flucRates(timescales_perG==300));
+Gfluc_std(t900) = std(flucRates(timescales_perG==900));
+Gfluc_std(t3600) = std(flucRates(timescales_perG==3600));
 
-Gfluc_norm_means(t30) = mean(flucRates_norm(timescales_perG==30));
-Gfluc_norm_means(t300) = mean(flucRates_norm(timescales_perG==300));
-Gfluc_norm_means(t900) = mean(flucRates_norm(timescales_perG==900));
-Gfluc_norm_means(t3600) = mean(flucRates_norm(timescales_perG==3600));
-
-Gfluc_norm_std(t30) = std(flucRates_norm(timescales_perG==30));
-Gfluc_norm_std(t300) = std(flucRates_norm(timescales_perG==300));
-Gfluc_norm_std(t900) = std(flucRates_norm(timescales_perG==900));
-Gfluc_norm_std(t3600) = std(flucRates_norm(timescales_perG==3600));
 
 
 % 4. plot G_data by timescale
 % raw values
 figure(2)
-plot([1 2 3 4],Gfluc_raw_means,'o','Color',rgb('DarkTurquoise'),'MarkerSize',10,'LineWidth',2);
+plot([1 2 3 4],Gfluc_means,'o','Color',rgb('DarkTurquoise'),'MarkerSize',10,'LineWidth',2);
 hold on
-errorbar([1 2 3 4],Gfluc_raw_means,Gfluc_raw_std,'Color',rgb('DarkTurquoise'));
+errorbar([1 2 3 4],Gfluc_means,Gfluc_std,'Color',rgb('DarkTurquoise'));
 hold on
-plot(-1, G_monod_raw,'o','Color',rgb('DarkCyan'),'MarkerSize',10,'LineWidth',2);
+plot(-1, G_monod,'o','Color',rgb('DarkCyan'),'MarkerSize',10,'LineWidth',2);
 hold on
-errorbar(-1,G_monod_raw,stableRates_raw_std(ave),'Color',rgb('DarkCyan'),'LineWidth',2);
+errorbar(-1,G_monod,stableRates_std(ave),'Color',rgb('DarkCyan'),'LineWidth',2);
 hold on
-plot(6, G_jensens_raw,'o','Color',rgb('SlateGray'),'MarkerSize',10,'LineWidth',2)
+plot(6, G_jensens,'o','Color',rgb('SlateGray'),'MarkerSize',10,'LineWidth',2)
 hold on
-axis([-1 6 0 12])
+axis([-1 6 0 4])
 title('growth expectations')
 xlabel('fluctuating timescale')
-ylabel('mean dV/dt (cubic um/hr)')
+ylabel('mean d(logV)/(dt*ln(2)) (1/hr)')
 
 
-% raw, normalized by G_jensens
+
+% normalized by G_jensens
 figure(3)
-plot([1 2 3 4],Gfluc_raw_means./G_jensens_raw,'o','Color',rgb('DarkTurquoise'),'MarkerSize',10,'LineWidth',2);
+plot([1 2 3 4],Gfluc_means./G_jensens,'o','Color',rgb('DarkTurquoise'),'MarkerSize',10,'LineWidth',2);
 hold on
-errorbar([1 2 3 4],Gfluc_raw_means./G_jensens_raw,Gfluc_raw_std./G_jensens_raw,'Color',rgb('DarkTurquoise'));
+errorbar([1 2 3 4],Gfluc_means./G_jensens,Gfluc_std./G_jensens,'Color',rgb('DarkTurquoise'));
 hold on
-plot(-1, G_monod_raw/G_jensens_raw,'o','Color',rgb('DarkCyan'),'MarkerSize',10,'LineWidth',2);
+plot(-1, G_monod/G_jensens,'o','Color',rgb('DarkCyan'),'MarkerSize',10,'LineWidth',2);
 hold on
-errorbar(-1,G_monod_raw/G_jensens_raw,stableRates_raw_std(ave)./G_jensens_raw,'Color',rgb('DarkCyan'),'LineWidth',2);
+errorbar(-1,G_monod/G_jensens,stableRates_std(ave)./G_jensens,'Color',rgb('DarkCyan'),'LineWidth',2);
 hold on
-plot(6, G_jensens_raw/G_jensens_raw,'o','Color',rgb('SlateGray'),'MarkerSize',10,'LineWidth',2)
+plot(6, G_jensens/G_jensens,'o','Color',rgb('SlateGray'),'MarkerSize',10,'LineWidth',2)
 
-axis([-1 6 0 1.2])
+axis([-1 6 0.3 1.5])
 title('growth, relative to Jensens expectations')
 xlabel('fluctuating timescale')
-ylabel('mean dV/dt, normalized to G_jensens')
+ylabel('mean d(logV)/(dt*ln(2)), normalized by G_jensens')
 
-
-% dVdt normalized by initial vol
-figure(4)
-plot([1 2 3 4],Gfluc_norm_means,'o','Color',rgb('DarkTurquoise'),'MarkerSize',10,'LineWidth',2);
-hold on
-errorbar([1 2 3 4],Gfluc_norm_means,Gfluc_norm_std,'Color',rgb('DarkTurquoise'));
-hold on
-plot(-1, G_monod_norm,'o','Color',rgb('DarkCyan'),'MarkerSize',10,'LineWidth',2);
-hold on
-errorbar(-1,G_monod_norm,stableRates_norm_std(ave),'Color',rgb('DarkCyan'),'LineWidth',2);
-hold on
-plot(6, G_jensens_norm,'o','Color',rgb('SlateGray'),'MarkerSize',10,'LineWidth',2)
-hold on
-axis([-1 6 0 2])
-title('growth expectations')
-xlabel('fluctuating timescale')
-ylabel('mean dV/dt / V (1/hr)')
-
-
-% dVdt normalized by initial vol, normalized by G_jensens
-figure(5)
-plot([1 2 3 4],Gfluc_norm_means./G_jensens_norm,'o','Color',rgb('DarkTurquoise'),'MarkerSize',10,'LineWidth',2);
-hold on
-errorbar([1 2 3 4],Gfluc_norm_means./G_jensens_norm,Gfluc_norm_std./G_jensens_norm,'Color',rgb('DarkTurquoise'));
-hold on
-plot(-1,G_monod_norm/G_jensens_norm,'o','Color',rgb('DarkCyan'),'MarkerSize',10,'LineWidth',2);
-hold on
-errorbar(-1,G_monod_norm/G_jensens_norm,stableRates_norm_std(ave)./G_jensens_norm,'Color',rgb('DarkCyan'),'LineWidth',2);
-hold on
-plot(6, G_jensens_norm/G_jensens_norm,'o','Color',rgb('SlateGray'),'MarkerSize',10,'LineWidth',2)
-
-axis([-1 6 0.2 1.4])
-title('growth, relative to Jensens expectations')
-xlabel('fluctuating timescale')
-ylabel('mean dV/dt / V, normalized to G_jensens')
 
 
 % dVdt normalized by initial vol, normalized by G_monod
-figure(6)
-plot([1 2 3 4],Gfluc_norm_means./G_monod_norm,'o','Color',rgb('DarkTurquoise'),'MarkerSize',10,'LineWidth',2);
+figure(4)
+plot([1 2 3 4],Gfluc_means./G_monod,'o','Color',rgb('DarkTurquoise'),'MarkerSize',10,'LineWidth',2);
 hold on
-errorbar([1 2 3 4],Gfluc_norm_means./G_monod_norm,Gfluc_norm_std./G_monod_norm,'Color',rgb('DarkTurquoise'));
+errorbar([1 2 3 4],Gfluc_means./G_monod, Gfluc_std./G_monod,'Color',rgb('DarkTurquoise'));
 hold on
-plot(-1,G_monod_norm/G_monod_norm,'o','Color',rgb('DarkCyan'),'MarkerSize',10,'LineWidth',2);
+plot(-1,G_monod./G_monod,'o','Color',rgb('DarkCyan'),'MarkerSize',10,'LineWidth',2);
 hold on
-errorbar(-1,G_monod_norm/G_monod_norm,stableRates_norm_std(ave)./G_monod_norm,'Color',rgb('DarkCyan'),'LineWidth',2);
+errorbar(-1,G_monod./G_monod,stableRates_std(ave)./G_monod,'Color',rgb('DarkCyan'),'LineWidth',2);
 hold on
-plot(6, G_jensens_norm/G_monod_norm,'o','Color',rgb('SlateGray'),'MarkerSize',10,'LineWidth',2)
+plot(6, G_jensens./G_monod,'o','Color',rgb('SlateGray'),'MarkerSize',10,'LineWidth',2)
 
-axis([-1 6 0 1.6])
+axis([-1 6 0.25 1.25])
 title('growth, relative to average nutrient concentration')
 xlabel('fluctuating timescale')
-ylabel('mean dV/dt / V, normalized to G_monod')
+ylabel('mean d(logV)/(dt*ln(2)), normalized by G_monod)')
 
-G_monod_norm
-G_jensens_norm
-Gfluc_norm_means
-Gfluc_norm_std
+G_monod
+G_jensens
+Gfluc_means
+Gfluc_std
