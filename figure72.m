3982c275eaa863caa96a238d359d92cea1c3ca46
Jen Nguyen <jknguyen@mit.edu>
2019-07-03 10:27:50 +0200

    stabilization of growth response from steady to fluc experiments

--- figure72.m
+++ figure72.m
@@ -1,4 +1,4 @@
-%  Figure 72 - successive period overal
+%%  Figure 72 - successive period overlay
 
 %  Goal: plot growth rate vs nutrient phase,
 %        binning rates by period fraction (20th of a period)
@@ -32,8 +32,8 @@
 
 
 
-%  Last edit: jen, 2019 May 20
-%  commit: first commit, looking for stabilization of growth response
+%  Last edit: jen, 2019 July 3
+%  commit: stabilization of growth response from steady to fluc experiments
 
 % Okie, go go let's go!
 
@@ -43,7 +43,7 @@ clear
 
 % 0. initialize analysis parameters
 condition = 1; % 1 = fluctuating; 3 = ave nutrient condition
-binsPerPeriod = 20;
+binsPerPeriod = 30;
 
 
 % 0. define growth rate of interest
@@ -54,11 +54,12 @@ specificColumn = 3;
 % 0. initialize complete meta data
 cd('/Users/jen/Documents/StockerLab/Data_analysis/')
 load('storedMetaData.mat')
-exptArray = [13,14,15]; % list experiments by index
+exptArray = [37,38]; % list experiments by index
 
 
 % 0. initialize colors per successive period
-colorSpectrum = {'Indigo','MediumSlateBlue','DodgerBlue','DeepSkyBlue','Teal','DarkGreen','MediumSeaGreen','GoldenRod','DarkOrange','Red'};
+%colorSpectrum = {'Indigo','MediumSlateBlue','DodgerBlue','DeepSkyBlue','Teal','DarkGreen','MediumSeaGreen','GoldenRod','DarkOrange','Red'};
+colorSpectrum = {'Indigo','DodgerBlue','DeepSkyBlue','Teal','MediumSeaGreen','GoldenRod','Red'};
 shape = '.';
 
 
@@ -77,6 +78,7 @@ for e = 1:length(exptArray)
     expType = storedMetaData{index}.experimentType;
     bubbletime = storedMetaData{index}.bubbletime;
     xys = storedMetaData{index}.xys;
+    shiftTime = storedMetaData{index}.shiftTime;
     
     disp(strcat(date, ': analyze!'))
     exptCounter = exptCounter + 1;
@@ -93,8 +95,13 @@ for e = 1:length(exptArray)
     
     % 4. gather specified condition data
     condition = 1; % 1 = fluctuating; 3 = ave nutrient condition
-    xy_start = storedMetaData{index}.xys(condition,1);
-    xy_end = storedMetaData{index}.xys(condition,end);
+    if strcmp(expType,'steady2fluc') == 1
+            xy_start = xys{condition}(1);
+            xy_end = xys{condition}(end);
+        else
+            xy_start = xys(condition,1);
+            xy_end = xys(condition,end);
+    end
     conditionData = buildDM(D5, T, xy_start, xy_end, index, expType);
     clear D5 T xy_start xy_end xys
     
@@ -115,7 +122,9 @@ for e = 1:length(exptArray)
     
     
     
-    % 7. generate a period vector by which to isolate data
+    % 7. trim timestamps and growth rate by bubble time
+    %     NOTE: errors (excessive negative growth rates) occur at trimming
+    %           point if growth rate calculation occurs AFTER time trim.
     timestamps_corrected = getGrowthParameter(conditionData,'correctedTime'); % signal corrected time
     if strcmp(date,'2017-10-10') == 1
         timestamps_corrected = timestamps_sec;
@@ -123,74 +132,90 @@ for e = 1:length(exptArray)
         
     timestamps_hr = timestamps_sec/3600;
     maxTime = bubbletime(condition);
-    timestamps_trimmed = timestamps_corrected(timestamps_hr <= maxTime);
-    growthRates_trimmed = growthRates(timestamps_hr <= maxTime,:);
-    period = ceil(timestamps_trimmed/3600); % current binning only works with 60 min period data
-    clear timestamps_hr timestamps_sec maxTime growthRates
+    
+    if maxTime ~= 0
+        timestamps_trimmed = timestamps_corrected(timestamps_hr <= maxTime);
+        growthRates_trimmed = growthRates(timestamps_hr <= maxTime,:);
+    else
+        timestamps_trimmed = timestamps_corrected;
+        growthRates_trimmed = growthRates;
+    end
+    
+    
+    
+    % 8. generate a bin vector for entire time series
+    %    assigning growth rate to time value of the middle of two timepoints (not end value)!
+    timestep_sec = 60+57;
+    timeInSeconds_middle = timestamps_trimmed - (timestep_sec/2);
+    timeInPeriods = timeInSeconds_middle/timescale;
+    binVector = ceil(timeInPeriods*binsPerPeriod);
+    clear timestamps_hr timestamps_sec maxTime growthRates timestamps_corrected
+    clear timeInPeriods timeInSeconds_middle timeInPeriods timestamps_trimmed timestep_sec
     
     
   
-    % 8. isolate growth rate of interest
+    % 9. isolate growth rate of interest and bin by timestamp
     growthRt = growthRates_trimmed(:,specificColumn); % log2
+    growthRt(binVector == 0) = [];
+    binVector(binVector == 0) = [];
+    growthRt_binned = accumarray(binVector,growthRt,[],@(x) {x});
     clear growthRates_trimmed
 
     
     
-    % 9. for each period, bin growth rates into 20ths of period timescale
-    for pp = 1:max(period)
+    % 10. initialize first period, based on shift time (time of fluctuation onset)
+    shiftBin = (shiftTime/timescale)*binsPerPeriod;
+    period_shift = [shiftBin-1, shiftBin:shiftBin+binsPerPeriod]; % includes two bins before upshift
+    period_first = period_shift - binsPerPeriod;
+    clear period_shift shiftBin
+    
+    
+    % 11. for each period, bin growth rates into period fraction
+    %numPeriods = max(ceil(binVector/binsPerPeriod));
+    for pp = 1:7 %numPeriods
+        
+        period_current = period_first + (30*(pp-1));
         
         
-        % 10. isolate data from current period
-        %     NOTE: errors (excessive negative growth rates) occur at trimming
-        %           point if growth rate calculation occurs AFTER time trim.
+        % 12. isolate data from current period
+        isEnd = period_current > length(growthRt_binned);
+        if sum(isEnd) > 0
+            period_current = period_current(isEnd == 0);
+        end
+        
+        period_mus = growthRt_binned(period_current);
 
-        periodMus = growthRt(period == pp);
-        periodTimes = timestamps_trimmed(period == pp);
         
-        
-        % 11. bin growth rates by 20th of period,
-        %     assigning growth rate to time value of the middle of two timepoints (not end value)!
-        timestep_sec = 60+57;
-        timeInSeconds_middle = periodTimes - (timestep_sec/2);
-        timeInPeriods = timeInSeconds_middle/timescale;    % units = sec/sec
-        timeInPeriods_floors = floor(timeInPeriods);
-        timeInPeriodFraction = timeInPeriods - timeInPeriods_floors;
-        assignedBin = timeInPeriodFraction * binsPerPeriod;
-        assignedBin = ceil(assignedBin);
-        
-        
-        periodMus_binned = accumarray(assignedBin,periodMus,[],@(x) {x});
-        clear timeInPeriods timeInPeriods_floors timeInPeriodFraction assignedBin timeInSeconds_middle
-        
-
    
-        % 12. calculate average growth rate per timebin
-        periodMus_means = cellfun(@nanmean,periodMus_binned);
-        periodMus_means_zeroed(pp,:) = [periodMus_means(end); periodMus_means];
-        
+        % 13. calculate average growth rate per timebin
+        signal_current = cellfun(@nanmean,period_mus);
+        signals_compiled{pp,:} = signal_current;
+
         
         
         % 13. plot
         color = rgb(colorSpectrum{pp});
         figure(e)
-        plot(periodMus_means_zeroed(pp,:),'Color',color,'LineWidth',1)
+        plot(signal_current,'Color',color,'LineWidth',1)
         hold on
         grid on
         title('growth rate: log2 mean')
-        xlabel('period bin (1/20)')
+        xlabel(strcat('period bin (',num2str(binsPerPeriod),' binsPerPeriod)'))
         ylabel('growth rate (1/hr)')
-        axis([1,21,-1,3])
+        axis([0,binsPerPeriod+3,-1,3])
         title(date)
+        
+        clear signal_current
     
         
     end
     
-    periodMus_compiled{exptCounter} = periodMus_means_zeroed;
-    clear periodMus_means_zeroed
+    signals_all{exptCounter} = signals_compiled;
+    clear signals_compiled
+    
     
 % 14. repeat for all experiments
 end
 
 
-
-
+%% C. compile replicate data with errorbars
